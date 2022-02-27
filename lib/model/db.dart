471ea1a751d69e19b4bb158a86407b1db4d7b0ba
Sidarzo <cyprien.rimbaud@orange.fr>
2022-02-27 19:30:46 +0100

    Db migration
    Db migration + delete exercise type model

--- lib/model/db.dart
+++ lib/model/db.dart
@@ -41,25 +41,18 @@ class dbFitNote {
           await database.execute(
               "INSERT INTO exercise_type VALUES (null, 'Musculation')");
         }
-        if (newVersion == 15) {
+        if (newVersion == 18) {
           await database.execute('DROP TABLE program');
-          await database.execute('DROP TABLE exercise_type');
-          await database.execute('DROP TABLE exercice');
-
-          var sql =
-              'CREATE TABLE program(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);';
-          sql +=
-              'CREATE TABLE exercise(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, program_id INTEGER NOT NULL,FOREIGN KEY(program_id) REFERENCES program(id));';
-          sql +=
-              'CREATE TABLE cardio(id INTEGER PRIMARY KEY AUTOINCREMENT, duration INTEGER NOT NULL, description TEXT NOT NULL,exercise_id INTEGER NOT NULL,FOREIGN KEY(exercise_id) REFERENCES exercise(id));';
-          sql +=
-              'CREATE TABLE muscu(id INTEGER PRIMARY KEY AUTOINCREMENT, weight INTEGER NOT NULL, repetition INTEGER NOT NULL, serie INTEGER NOT NULL, restDuration INTEGER NOT NULL, exercise_id INTEGER NOT NULL,FOREIGN KEY(exercise_id) REFERENCES exercise(id));';
-          await database.execute(sql);
+          await database.execute('CREATE TABLE program(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)');
+          await database.execute('CREATE TABLE exercise(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, program_id INTEGER NOT NULL,FOREIGN KEY(program_id) REFERENCES program(id))');
+          await database.execute('CREATE TABLE cardio(id INTEGER PRIMARY KEY AUTOINCREMENT, duration INTEGER NOT NULL, description TEXT NOT NULL,exercise_id INTEGER NOT NULL,FOREIGN KEY(exercise_id) REFERENCES exercise(id))');
+          await database.execute('CREATE TABLE muscu(id INTEGER PRIMARY KEY AUTOINCREMENT, weight INTEGER NOT NULL, repetition INTEGER NOT NULL, serie INTEGER NOT NULL, restDuration INTEGER NOT NULL, exercise_id INTEGER NOT NULL,FOREIGN KEY(exercise_id) REFERENCES exercise(id))');
+         
         }
 
         await batch.commit();
       },
-      version: 15,
+      version: 18,
     );
   }
 }
