a485d0679f9b6840e93cd4d611a232d16b709c8b
Sidarzo <cyprien.rimbaud@orange.fr>
2022-04-28 16:59:25 +0200

    Update insert program

--- lib/view_model/program/programform_view_model.dart
+++ lib/view_model/program/programform_view_model.dart
@@ -6,9 +6,7 @@ class ProgramFormViewModel extends ChangeNotifier {
   ProgramFormViewModel();
 
   void createProgramButtonOnClickCommand(String name) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int userId = prefs.getInt('userId') ?? 0;
-
-    // Program.insertProgram(userId, name);
+    Program program = Program(id: null, name: name);
+    Program.insertProgram(program);
   }
 }
