9b699fa40a6856cf448fdbc86b8ae6931580efbd
Sidarzo <cyprien.rimbaud@orange.fr>
2022-08-30 00:13:00 +0200

    Change landing view

--- lib/view/landing_view.dart
+++ lib/view/landing_view.dart
@@ -26,27 +26,25 @@ class _LandingViewState extends State<LandingView> {
   }
 
   Widget build(BuildContext context) {
-    final applicationName = Container(
-      color: Colors.purpleAccent,
-      padding: const EdgeInsets.all(10),
-      child: const Text(
-        'FitNote',
-        style: TextStyle(
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
-          color: Colors.purple,
-        ),
-      ),
-    );
+    
 
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 32, 32, 32),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            applicationName,
-            const Center(),
-          ],
-        ));
+        body: Container(
+          decoration: 
+            const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment(0.9, 1),
+                  colors: <Color>[
+                   Color.fromARGB(255, 191, 250, 236),
+                   Color.fromARGB(255, 170, 250, 231),
+                   Color.fromARGB(255, 160, 253, 233),
+                ], // Gradient from https://learnui.design/tools/gradient-generator.html
+            tileMode: TileMode.clamp,
+          ),
+        ),
+      ) 
+    );
   }
 }
